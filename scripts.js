0d91fc5847e885f4cc7ab2c28dec96add7cc6cf6
Sarit Soponvattanaevi <sarits.byc@gmail.com>
2021-07-07 16:02:58 +0700

    update style

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-$(window).on('beforeunload', function () {
-  $('body').hide();
-  $(window).scrollTop(0);
-});
+// This prevents the page from scrolling down to where it was previously.
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+// This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+window.scrollTo(0, 0);
