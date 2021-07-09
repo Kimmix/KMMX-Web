c293fe2bf5d5e51a48604d4847283eb0af0818c5
Sarit Soponvattanaevi <sarits.byc@gmail.com>
2021-07-09 20:36:26 +0700

    pwa working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 const logoFill = document.getElementById("logoFill");
 const title1 = document.getElementById("title1");
 const block2 = document.getElementById("block2");
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const cacheName = "kmmx-site-v1"
+const assets = [
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/asset/logo.svg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
