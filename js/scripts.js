63d88602f624c0672a324fdacd01cfa5ba7c5f44
Sarit Soponvattanaevi <sarits.byc@gmail.com>
2021-07-09 20:06:30 +0700

    optimize

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 const logoFill = document.getElementById("logoFill");
 const title1 = document.getElementById("title1");
+const block2 = document.getElementById("block2");
 
 window.addEventListener('scroll', function() {
   const yPosition = window.scrollY
-  logoFill.style.top = yPosition * 0.5 + 'px';
-  title1.style.transform = `translateX(${yPosition * 0.5}px)`;
+  // console.log(yPosition);
+  if (yPosition < 600) {
+    logoFill.style.top = yPosition * 1.5 + 'px';
+    title1.style.transform = `translateX(${yPosition * 0.5}px)`;
+  }
   // transform: translateX(-17px);
 })
