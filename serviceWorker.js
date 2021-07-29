7fe9fef6d2cbe4074ca37495e07e25b20c543ee3
Kimmix <kimmix05@gmail.com>
2021-07-29 11:44:05 +0700

    update style

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,9 +16,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets)
-    })
+    caches.open(cacheName).then(cache => { cache.addAll(assets) })
   )
 })
 
