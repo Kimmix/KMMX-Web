f5463e65ac3f7dd83a4eacdf0f080848b9ca16a8
Kimmix <kimmix05@gmail.com>
2021-08-01 11:43:37 +0700

    update footer

--- asset/script/app.js
+++ asset/script/app.js
@@ -52,3 +52,13 @@ const animationObserver = new IntersectionObserver((entries, observer) => {
 
 animationObserver.observe(document.getElementById('logo-stroke'));
 animationObserver.observe(document.getElementById('stardate'));
+
+// Footer
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function onScrollTo(id) {
+  document.getElementById(id).scrollIntoView(true);
+}
