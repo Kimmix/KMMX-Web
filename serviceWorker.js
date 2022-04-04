7c24c765bdefbfc78fa0b9e02720c8cbb4b4d5e7
Kimmix <kimmix05@gmail.com>
2022-04-04 16:47:22 +0700

    less caching

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,8 @@
-const cacheName = 'kmmx-site-v1.2.5'
+const cacheName = 'kmmx-site-v1.2.6'
 const assets = [
-  "/index.html",
   "/favicon.png",
   "/asset/script/app.js",
   "/asset/script/cursor.js",
-  "/asset/Katana.png",
   "/asset/KIMMIX_CHIBI.webp",
   "/asset/KMMX_inthebox.webp",
   "/asset/KMMX_Katana2.webp",
@@ -13,14 +11,14 @@ const assets = [
   "/asset/splash.png",
   "/asset/arcai_b.svg",
   "/css/cursor.css",
-  "/css/styles-archive.css",
-  "/css/styles-cardui.css",
-  "/css/styles-contact.css",
-  "/css/styles-footer.css",
-  "/css/styles-main.css",
-  "/css/styles-refsheet.css",
-  "/css/styles-title.css",
 ]
+// "/css/styles-archive.css",
+// "/css/styles-cardui.css",
+// "/css/styles-contact.css",
+// "/css/styles-footer.css",
+// "/css/styles-main.css",
+// "/css/styles-refsheet.css",
+// "/css/styles-title.css",
 
 self.addEventListener("install", async installEvent => {
   await installEvent.waitUntil(
