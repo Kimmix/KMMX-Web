e48ac000780095b550fb399e479fe1bc808f1cd0
Kimmix <kimmix05@gmail.com>
2022-10-01 18:24:54 +0700

    testing cinematic

---
+++ asset/script/app_c.js
@@ -0,0 +1,9 @@
+// // Animation triggering
+// const animationObserver = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     entry.target.classList.toggle('animate-onscroll', entry.isIntersecting)
+//   })
+// }, {
+//   threshold: 1,
+// });
+// animationObserver.observe(document.getElementById('job-label'));
