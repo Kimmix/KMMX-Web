47dfc552e943b126678d58da9213b1578aa84588
Kimmix <kimmix05@gmail.com>
2024-10-21 21:50:31 +0700

    add external lib

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// Initialize Lenis
+const lenis = new Lenis()
+
+// Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 // Check device type
 function getDeviceType() {
     const ua = navigator.userAgent;
