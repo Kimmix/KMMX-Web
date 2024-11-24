5afa5740740d74beb641cd89f96f14cdbb7ac1ef
Kimmix <kimmix05@gmail.com>
2024-11-24 18:07:58 +0700

    update character

--- js/main.js
+++ js/main.js
@@ -196,3 +196,14 @@ function showContent(sectionId, event) {
     document.getElementById(sectionId).classList.add('active');
     event.target.classList.add('active');
 }
+
+document.querySelectorAll('.stat-bar').forEach((div) => {
+    const position = div.getAttribute('data-position'); // Get the position from data attribute
+    // div.style.background = `linear-gradient(
+    //   90deg,
+    //   rgba(203, 32, 64, 1) 0%,
+    //   rgba(137, 59, 133, 1) ${position},
+    //   rgba(60, 64, 93, 1) 100%
+    // )`;
+    div.style.background = `linear-gradient(90deg, #cb2040 0%, #893b85 ${position}, #3c405d 100%)`;
+  });
